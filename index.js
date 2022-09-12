0a021244038d27015d81bd0713c1f9a87c562ef4
Chappydev <danjtchapman@gmail.com>
2022-09-11 20:22:17 -0600

    Complete 4.2; move app to app.js and update index.js to accomodate the change

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const logger = require('./utils/logger');
+const http = require('http');
+const app = require('./app');
 const config = require('./utils/config');
-const Blog = require('./models/blog');
-const mongoose = require('mongoose');
-const blogsRouter = require('./controllers/blogs');
-const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
 
-mongoose.connect(config.MONGODB_URI);
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use('/api/blogs', blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
