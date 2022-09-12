6b65940a3b39d330313d5443ce83c549c909007b
Chappydev <danjtchapman@gmail.com>
2022-09-11 20:14:14 -0600

    Add middleware functions

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const config = require('./utils/config');
 const Blog = require('./models/blog');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
 
 mongoose.connect(config.MONGODB_URI);
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 
 app.listen(config.PORT, () => {
