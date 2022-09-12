a614d29d726c7b1630467f22efcdda00debe14ef
Chappydev <danjtchapman@gmail.com>
2022-09-11 20:01:43 -0600

    Move routing to controllers/blogs.js

--- index.js
+++ index.js
@@ -6,29 +6,14 @@ const logger = require('./utils/logger');
 const config = require('./utils/config');
 const Blog = require('./models/blog');
 const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
 
 mongoose.connect(config.MONGODB_URI);
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter);
 
 
 app.listen(config.PORT, () => {
