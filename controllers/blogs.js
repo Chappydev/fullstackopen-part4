df3ec4249d29bc8eba88d52826dc78e094540edb
Chappydev <danjtchapman@gmail.com>
2022-09-15 13:24:01 -0600

    Complete 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 });
   
   response.json(blogs);
 });
@@ -16,15 +19,22 @@ blogsRouter.post('/', async (request, response) => {
     });
   }
 
+  const user = await User.findOne({});
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id
   });
 
-  const result = await blog.save()
-  response.status(201).json(result);
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  console.log(user);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
